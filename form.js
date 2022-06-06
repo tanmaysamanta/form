f38a72819c5eb237d9ae79fbb1558286176e93ab
Tanmay S <99489689+tanmaysamanta@users.noreply.github.com>
2022-06-06 15:16:53 +0530

    Addded createForm and updateInfo function

---
+++ form.js
@@ -0,0 +1,30 @@
+// const { EventEmitter } = require('events');
+
+
+const updateInfo = (form, question, info) => {
+  const questionDetails = question.split(' ');
+  const query = questionDetails[questionDetails.length - 1];
+  form[query] = info;
+}
+
+const createForm = () => {
+  const queries = [
+    'Please enter your name:',
+    'Please enter your DOB:',
+    'Please enter your hobbies'
+  ];
+  let index = 0;
+  let form = {};
+  process.stdin.setEncoding('utf8');
+  process.stdin.on('data', (chunk) => {
+    const info = chunk.trim();
+    from = updateInfo(form, queries[index], chunk);
+    index++;
+    if (index >= queries.length) {
+      console.log(form);
+      process.exit();
+    }
+  });
+};
+
+createForm();
